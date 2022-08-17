7db630d8a5ae2afd222ab461fe29431cdea4ac1b
Gleb Kostyakov <kosgleb@gmail.com>
2022-08-17 10:33:51 +0700

    HW3 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,44 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(input string) []string {
+	if input == "" {
+		return []string{}
+	}
+
+	wordsArray := strings.Fields(input)
+	wordsMap := map[string]int{}
+
+	for i := 0; i < len(wordsArray); i++ {
+		word := wordsArray[i]
+		if _, ok := wordsMap[word]; ok {
+			wordsMap[word]++
+		} else {
+			wordsMap[word] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(wordsMap))
+
+	for k := range wordsMap {
+		keys = append(keys, k)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		if wordsMap[keys[i]] > wordsMap[keys[j]] {
+			return true
+		}
+
+		if wordsMap[keys[i]] < wordsMap[keys[j]] {
+			return false
+		}
+
+		return keys[i] < keys[j]
+	})
+
+	return keys[0:10]
 }
